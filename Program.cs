a3b3ea8bfbe5ef272c054b96c1bb082172df5570
Zach McWhirter <zmac7811@gmail.com>
2020-08-25 12:13:04 -0500

    Phase 2 complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace GuessingGame
 {
@@ -8,12 +10,22 @@ namespace GuessingGame
         {
             //Type 
             Console.WriteLine("Guess a secret number between 1 and 100:");
-
+            //Secret Number to be guessed by user
+            int secretNum = 42;
             // Create a string variable and get user input from the keyboard and store it in the variable
             string userGuess = Console.ReadLine();
             // Print the value of the variable (userGuess), which will display the input value
-            Console.WriteLine($"You guessed: {userGuess}");
-        }
+            // Console.WriteLine($"You guessed: {userGuess}");
 
+            int parsedNum = Int32.Parse(userGuess);
+            if (secretNum == parsedNum)
+            {
+                Console.WriteLine("Correct!");
+            }
+            else
+            {
+                Console.WriteLine("Nope");
+            }
+        }
     }
 }
