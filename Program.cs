079fc9bac1ab04cdda3d7ce1989f408d13ab3ae5
Zach McWhirter <zmac7811@gmail.com>
2021-04-18 17:25:30 -0500

    stage 5 complete

--- Program.cs
+++ Program.cs
@@ -8,37 +8,41 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            //Type 
+
             Console.WriteLine("Guess a secret number between 1 and 100:");
+            Console.WriteLine("");
+            
+            // Create a Random object
+            Random random = new Random();
+            // Then use it to generate a random Secret Number to be guessed by user
+            int secretNum = random.Next(1, 101);
 
-            //Secret Number to be guessed by user
-            int secretNum = 42;
-
-            //Initiate the guess counter at 1
+            // Initiate the guess counter at 1
             int numOfGuesses = 0;
 
-            // while (numOfGuesses <= 3)
-
-            //Limit number of guesses to 4
+            // Limit number of guesses to 4
             for (int i = 0; i < 4; i++)
 
             {
                 // Create a string variable and get user input from the keyboard and store it in the variable
                 string userNum = Console.ReadLine();
 
-                // parsedUserNum = Int32.Parse(Console.ReadLine());
-
-                //Parse the user input string to get an integer
+                // Parse the user input string to get an integer
                 int parsedUserNum = Int32.Parse(userNum);
 
+                // Increment number of guesses by 1
                 numOfGuesses++;
+
                 if (secretNum != parsedUserNum)
                 {
-                    Console.WriteLine($"Your guess: {numOfGuesses}");
-                    Console.WriteLine("Nope");
+                    Console.WriteLine($"Nope, Its not {parsedUserNum}!");
+                    Console.WriteLine($"{4 - numOfGuesses} guesses remaining");
                     if (numOfGuesses != 4)
                     {
-                        Console.WriteLine($"Guess again!");
+                        Console.WriteLine("Guess again!");
+                        Console.WriteLine("");
+                        Console.WriteLine("------------");
+                        Console.WriteLine("");
                     }
                 }
                 else
