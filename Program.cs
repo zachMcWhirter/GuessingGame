e3797bdf4dc9153552cbf43670bfd26e40365234
Zach McWhirter <zmac7811@gmail.com>
2021-04-18 18:08:30 -0500

    add difficulty levels

--- Program.cs
+++ Program.cs
@@ -8,6 +8,35 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Choose your difficulty level: ");
+            Console.WriteLine("1  for EASY (8 guesses)");
+            Console.WriteLine("2  for MEDIUM (6 guesses)");
+            Console.WriteLine("3  for HARD (4 guesses)");
+
+            // Initiate the guess counter at 1
+            int numOfGuesses = 0;
+
+            string difficulty = Console.ReadLine();
+
+            if (difficulty == "1")
+            {
+                numOfGuesses = 8;
+            }
+            else if (difficulty == "2")
+            {
+                numOfGuesses = 6;
+            }
+            else if (difficulty == "3")
+            {
+                numOfGuesses = 4;
+            }
+            else
+            {
+                Console.WriteLine("Choose your difficulty level: ");
+                Console.WriteLine("1  for EASY (8 guesses)");
+                Console.WriteLine("2  for MEDIUM (6 guesses)");
+                Console.WriteLine("3  for HARD (4 guesses)");
+            }
 
             Console.WriteLine("Guess a secret number between 1 and 100:");
             Console.WriteLine("");
@@ -17,21 +46,20 @@ namespace GuessingGame
             // Then use it to generate a random Secret Number to be guessed by user
             int secretNum = random.Next(1, 101);
 
-            // Initiate the guess counter at 1
-            int numOfGuesses = 0;
+            ;
 
             // Limit number of guesses to 6
-            for (int i = 0; i < 6; i++)
+            while (numOfGuesses > 0)
 
             {
-                // Create a string variable and get user input from the keyboard and store it in the variable
+                // Create a string variable to get user input and store it as userNum
                 string userNum = Console.ReadLine();
 
                 // Parse the user input string to get an integer
                 int parsedUserNum = Int32.Parse(userNum);
 
                 // Increment number of guesses by 1
-                numOfGuesses++;
+                numOfGuesses--;
 
                 if (secretNum == parsedUserNum)
                 {
@@ -42,8 +70,8 @@ namespace GuessingGame
                 else if (secretNum < parsedUserNum)
                 {
                     Console.WriteLine($"Nope, Its not {parsedUserNum}!");
-                    Console.WriteLine($"{6 - numOfGuesses} guesses remaining");
-                    if (numOfGuesses != 6)
+                    Console.WriteLine($"{numOfGuesses} guesses remaining");
+                    if (numOfGuesses != 0)
                     {
                         Console.WriteLine("Guess Lower!");
                         Console.WriteLine("");
@@ -54,8 +82,8 @@ namespace GuessingGame
                 else if (secretNum > parsedUserNum)
                 {
                     Console.WriteLine($"Nope, Its not {parsedUserNum}!");
-                    Console.WriteLine($"{6 - numOfGuesses} guesses remaining");
-                    if (numOfGuesses != 6)
+                    Console.WriteLine($"{numOfGuesses} guesses remaining");
+                    if (numOfGuesses != 0)
                     {
                         Console.WriteLine("Guess Higher!");
                         Console.WriteLine("");
